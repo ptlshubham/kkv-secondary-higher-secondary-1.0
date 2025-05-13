74bc111a3d9f718b2228ac8192b7c5e2d67d3a55
Shubham Patel <ptlshubham@gmail.com>
2025-05-13 14:13:06 +0530

    theme

---
+++ src/app/basic/basic.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InfrastructureComponent } from './infrastructure/infrastructure.component';
+import { BasicRoutes } from './basic.routing';
+import { SharedModule } from '../shared/shared.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { ActivityComponent } from './activity/activity.component';
+import { ContactComponent } from './contact/contact.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AboutComponent } from './about/about.component';
+import { AchievementsComponent } from './achievements/achievements.component';
+import { NgxPaginationModule } from 'ngx-pagination';
+
+
+
+@NgModule({
+  declarations: [
+    InfrastructureComponent,
+    ActivityComponent,
+    ContactComponent,
+    AboutComponent,
+    AchievementsComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule.forChild(BasicRoutes),
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxPaginationModule,
+  ]
+})
+export class BasicModule { }
